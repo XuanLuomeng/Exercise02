39e7f81120b8cfe7af8348cff75dd0b03d5033de
XuanLuomeng <1079716693@qq.com>
2022-09-14 23:23:16 +0800

    seventeenth

---
+++ src/main/java/day06/linkedList/LinkedList.java
@@ -0,0 +1,7 @@
+package day06.linkedList;
+
+public class LinkedList {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/day06/recursion/FindMax.java
@@ -0,0 +1,42 @@
+package day06.recursion;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FindMax {
+    public static void main(String[] args) throws IOException {
+        /**
+         * Get the array by reading and writing the file
+         */
+        int maxNum = 0;
+        String[] strArray;
+        int[] array = new int[100];
+        BufferedReader br = new BufferedReader(new FileReader("src/main/java/day06/recursion/datafile.txt"));
+        String line;
+        while ((line = br.readLine()) != null) {
+            strArray = line.split(",");
+            for (int i = 0; i < 100; i++) {
+                array[i] = Integer.parseInt(strArray[i]);
+            }
+        }
+        maxNum = findMax(array, 0);
+        System.out.println(maxNum);
+    }
+
+    /**
+     * Each recursion returns a maximum number
+     *
+     * @param array
+     * @param num
+     * @return
+     */
+    public static int findMax(int[] array, int num) {
+        int max = 0;
+        if (num < array.length) {
+            int nextNum = findMax(array, num + 1);
+            max = array[num] > nextNum ? array[num] : nextNum;
+        }
+        return max;
+    }
+}
