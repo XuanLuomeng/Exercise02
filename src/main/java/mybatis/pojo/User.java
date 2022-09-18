f3bb0649405723d15a7db986a63666944e0b11e0
XuanLuomeng <1079716693@qq.com>
2022-09-18 22:59:40 +0800

    twentieth

--- src/main/java/mybatis/pojo/User.java
+++ src/main/java/mybatis/pojo/User.java
@@ -14,4 +14,13 @@ public class User {
     private Integer age;
     private String sex;
     private String email;
+
+    public User(Integer id, String username, String password, Integer age, String sex, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.age = age;
+        this.sex = sex;
+        this.email = email;
+    }
 }
