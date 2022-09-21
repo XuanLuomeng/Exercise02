1f708af0cd7632c50f87912d60d0186ba046f1c2
XuanLuomeng <1079716693@qq.com>
2022-09-21 23:32:37 +0800

    twenty-third

--- src/main/java/mybatis/mapper/SpecisalSQLMapper.java
+++ src/main/java/mybatis/mapper/SpecisalSQLMapper.java
@@ -12,4 +12,23 @@ public interface SpecisalSQLMapper {
      * @return
      */
     List<User> getUserByLike(@Param("mohu") String mohu);
+
+    /**
+     * 批量删除
+     * @param ids
+     */
+    void deleteMoreUser(@Param("ids") String ids);
+
+    /**
+     * 动态设置表名，来查询用户信息
+     * @param tableName
+     * @return
+     */
+    List<User> getUserList(@Param("tableName")String tableName);
+
+    /**
+     * 添加用户信息并获取自增的主键
+     * @param user
+     */
+    void insertUser(User user);
 }
--- src/test/java/mybatis/test/SpecialSQLMapperTest.java
+++ src/test/java/mybatis/test/SpecialSQLMapperTest.java
@@ -6,14 +6,12 @@ import mybatis.utils.SqlSessionUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.*;
 import java.util.List;
 
 public class SpecialSQLMapperTest {
     @Test
-    public void testGetUserByLike(){
+    public void testGetUserByLike() {
         SqlSession sqlSession = SqlSessionUtil.getSqlSession();
         SpecisalSQLMapper mapper = sqlSession.getMapper(SpecisalSQLMapper.class);
         List<User> list = mapper.getUserByLike("a");
@@ -21,15 +19,46 @@ public class SpecialSQLMapperTest {
     }
 
     @Test
-    public void testJDBC(){
+    public void testJDBC() {
         try {
             Class.forName("");
-            Connection connection = DriverManager.getConnection("","","");
-            String sql = "select * from t_user where '%?%'";
-            PreparedStatement ps = connection.prepareStatement(sql);
-            //ps.setString(1,"a");
-        }catch (Exception e){
+            Connection connection = DriverManager.getConnection("", "", "");
+//            String sql = "select * from t_user where '%?%'";
+//            PreparedStatement ps = connection.prepareStatement(sql);
+//            //ps.setString(1,"a");
+            String sql = "insert into t_user values()";
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.executeUpdate();
+            ResultSet resultSet = ps.getGeneratedKeys();
+            resultSet.next();
+            int id = resultSet.getInt(1);
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void testDeleteMoreUser() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        SpecisalSQLMapper mapper = sqlSession.getMapper(SpecisalSQLMapper.class);
+        mapper.deleteMoreUser("9,10");
+    }
+
+    @Test
+    public void testGetUserList() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        SpecisalSQLMapper mapper = sqlSession.getMapper(SpecisalSQLMapper.class);
+        List<User> list = mapper.getUserList("t_user");
+        list.forEach(System.out::println);
+
+    }
+
+    @Test
+    public void testInsertUser() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        SpecisalSQLMapper mapper = sqlSession.getMapper(SpecisalSQLMapper.class);
+        User user = new User(null,"xiaoming","123456",23,"男","1234567@qq.com");
+        mapper.insertUser(user);
+        System.out.println(user);
     }
 }
