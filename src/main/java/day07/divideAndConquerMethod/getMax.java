24cb87c47a78959b9522ad7b352ed699e37d7558
XuanLuomeng <1079716693@qq.com>
2022-09-22 19:12:37 +0800

    twenty-forth

---
+++ src/main/java/day07/divideAndConquerMethod/getMax.java
@@ -0,0 +1,4 @@
+package day07.divideAndConquerMethod;
+
+public class getMax {
+}
---
+++ src/main/java/day07/linkedList/LinkNode.java
@@ -0,0 +1,19 @@
+package day07.linkedList;
+
+/**
+ * Initialization and assignment of each object
+ * @param <T>
+ */
+public class LinkNode<T> {
+    T data;
+    LinkNode<T> next;
+
+    public LinkNode() {
+        next = null;
+    }
+
+    public LinkNode(T d) {
+        data = d;
+        next = null;
+    }
+}
