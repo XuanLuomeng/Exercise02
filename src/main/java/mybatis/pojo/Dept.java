d90451dac520b510005820821f98dfceaccd6aa6
XuanLuomeng <1079716693@qq.com>
2022-09-25 17:02:09 +0800

    twenty-sixth

--- src/main/java/mybatis/pojo/Dept.java
+++ src/main/java/mybatis/pojo/Dept.java
@@ -2,16 +2,19 @@ package mybatis.pojo;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Dept {
-    private Integer depId;
+    private Integer deptId;
     private String deptName;
+    private List<Emp> emps;
 
     public Dept() {
     }
 
-    public Dept(Integer depId, String deptName) {
-        this.depId = depId;
+    public Dept(Integer deptId, String deptName) {
+        this.deptId = deptId;
         this.deptName = deptName;
     }
 }
