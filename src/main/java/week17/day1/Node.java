38ab1e57fe24b590ed78c323576873cd88b2b9a7
luojiawei <1079716693@qq.com>
2025-10-05 21:04:18 +0800

    2025-10-05

---
+++ src/main/java/week17/day1/Node.java
@@ -0,0 +1,19 @@
+package week17.day1;
+
+/**
+ * @author LuoXuanwei
+ * @version 1.0
+ * @description TODO
+ * @date 2025/10/5 20:46
+ */
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
